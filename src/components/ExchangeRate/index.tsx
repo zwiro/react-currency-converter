2e4ebd8dde7787035bbe99cd18aa38d66c2f9f1f
zwiro <josiu100@gmail.com>
2023-08-12 14:05:26 +0200

    Displaying converted from / to currencies in ExchangeRate component

--- src/components/ExchangeRate/index.tsx
+++ src/components/ExchangeRate/index.tsx
@@ -1,7 +1,18 @@
+import { useAtom } from "jotai"
 import "./exchange-rate.scss"
+import {
+  convertedFromCurrencyAtom,
+  convertedToCurrencyAtom,
+} from "@/jotai/atoms"
 
 const ExchangeRate = () => {
-  return <h2 className="exchange-rate | text-center py-9">1 PLN = 4 EUR</h2>
+  const [convertedFromCurrency] = useAtom(convertedFromCurrencyAtom)
+  const [convertedToCurrency] = useAtom(convertedToCurrencyAtom)
+  return (
+    <h2 className="exchange-rate | text-center py-9">
+      1 {convertedFromCurrency} = 4 {convertedToCurrency}
+    </h2>
+  )
 }
 
 export default ExchangeRate
