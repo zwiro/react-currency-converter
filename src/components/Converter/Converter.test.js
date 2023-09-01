182bbefb0a1f52a9e1395cd0c676d1b2dad64c48
zwiro <josiu100@gmail.com>
2023-09-01 14:56:02 +0200

    Added tests for validating inputs

--- src/components/Converter/Converter.test.js
+++ src/components/Converter/Converter.test.js
@@ -109,3 +109,91 @@ test("when swapping input values, swapped value is converted", async () => {
 
   expect(input1).toHaveValue(mockValue)
 })
+
+test("user can't type more than one dot in input", async () => {
+  render(<Converter />)
+
+  const input1 = await screen.findByRole("textbox", {
+    name: /type the value to convert \(1\)/i,
+  })
+
+  const input2 = await screen.findByRole("textbox", {
+    name: /type the value to convert \(2\)/i,
+  })
+
+  const mockValue = "0.."
+
+  await user.type(input1, mockValue)
+  expect(input1).toHaveValue("0.")
+
+  fireEvent.change(input2, { target: { value: "" } })
+
+  await user.type(input2, mockValue)
+  expect(input2).toHaveValue("0.")
+})
+
+test("user can't type 0 before other numbers in integer part", async () => {
+  render(<Converter />)
+
+  const input1 = await screen.findByRole("textbox", {
+    name: /type the value to convert \(1\)/i,
+  })
+
+  const input2 = await screen.findByRole("textbox", {
+    name: /type the value to convert \(2\)/i,
+  })
+
+  const mockValue = "005"
+
+  await user.type(input1, mockValue)
+  expect(input1).toHaveValue("5")
+
+  fireEvent.change(input2, { target: { value: "" } })
+
+  await user.type(input2, mockValue)
+  expect(input2).toHaveValue("5")
+})
+
+test("user can't type anything but numbers and dot in input", async () => {
+  render(<Converter />)
+
+  const input1 = await screen.findByRole("textbox", {
+    name: /type the value to convert \(1\)/i,
+  })
+
+  const input2 = await screen.findByRole("textbox", {
+    name: /type the value to convert \(2\)/i,
+  })
+
+  const mockValue = "asdfg!@#$%^&*()_+12.34"
+
+  await user.type(input1, mockValue)
+  expect(input1).toHaveValue("12.34")
+
+  fireEvent.change(input2, { target: { value: "" } })
+
+  await user.type(input2, mockValue)
+  expect(input2).toHaveValue("12.34")
+})
+
+test("0 is adding automatically when user types dot in empty input", async () => {
+  render(<Converter />)
+
+  const input1 = await screen.findByRole("textbox", {
+    name: /type the value to convert \(1\)/i,
+  })
+
+  const input2 = await screen.findByRole("textbox", {
+    name: /type the value to convert \(2\)/i,
+  })
+
+  const mockValue = "."
+
+  await user.type(input1, mockValue)
+  expect(input1).toHaveValue("0.")
+
+  fireEvent.change(input2, { target: { value: "" } })
+
+  await user.type(input2, mockValue)
+  expect(input2).toHaveValue("0.")
+})
